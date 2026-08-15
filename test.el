38dcadc9cca100708f098736cd8c71977c88ea9e
Mats Lidell <mats.lidell@lidells.se>
2026-08-15 15:23:37 +0200

    First attempt

---
+++ test.el
@@ -0,0 +1,8 @@
+;; -*- lexical-binding: t; -*-
+
+(require 'ert)
+
+(ert-deftest test-package-install ()
+  "Test package install."
+  (package-refresh-contents)
+  (package-install 'markdown-mode))
