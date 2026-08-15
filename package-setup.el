50ac032f175fdbc1135b4cab7448ec7ed5200ece
Mats Lidell <mats.lidell@lidells.se>
2026-08-15 15:52:05 +0200

    Refresh before test

--- package-setup.el
+++ package-setup.el
@@ -1,3 +1,8 @@
 ;; -*- lexical-binding: t; -*-
 
 (require 'package)
+(setq package-pinned-packages
+      '((markdown-mode . "nongnu")))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(package-refresh-contents)
